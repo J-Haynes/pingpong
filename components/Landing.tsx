999c109132490be45640d3542fa8110ccfb09b41
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-14 11:43:29 +1200

    styling almost done

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,20 +1,47 @@
 import React from 'react'
-import { Button, Image, Text, View, StyleSheet } from 'react-native'
+import { Button, Image, Text, View, StyleSheet, Pressable } from 'react-native'
 
 export default function Landing({ navigation }: any) {
   return (
     <View style={styles.container}>
-      <Image source={require('../assets/favicon.png')}></Image>
-      <Text>PINGPONG</Text>
-      <Text>taking the social out of social media</Text>
-      <Button
-        title="Log In / Sign Up"
+      <Image
+        style={styles.image}
+        source={require('../assets/ball.png')}
+      ></Image>
+      <View>
+        <Text style={styles.title}>PINGPONG</Text>
+        <Text style={styles.mainText}>
+          taking the media out of social media
+        </Text>
+      </View>
+      <Pressable
+        style={styles.button}
         onPress={() => navigation.navigate('Ping')}
-      ></Button>
+      >
+        <Text style={styles.buttonText}>LOG IN / SIGN UP</Text>
+      </Pressable>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#fde4cf',
+  },
+  title: { color: 'black', fontSize: 50, alignSelf: 'center' },
+  mainText: { color: 'black', fontSize: 20, alignSelf: 'center' },
+  buttonText: { color: 'black', fontSize: 30, alignSelf: 'center' },
+  button: {
+    backgroundColor: '#d0d9b3',
+    padding: 10,
+    borderRadius: 5,
+  },
+  image: { width: 100, height: 100 },
 })
+
+// champagne = #fde4cf
+// yellow = #f8f8cc
+// green = #fde4cf
