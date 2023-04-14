cb3360a2872787046a1406c783678cd4cd953907
student <student@pop-os.localdomain>
2023-04-14 16:01:41 +1200

    add functionality to show active and inactive pings on friends component

--- components/Friends.tsx
+++ components/Friends.tsx
@@ -1,10 +1,64 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, SectionList } from 'react-native'
+import ActiveFriend from './ActiveFriend'
+import BasicFriend from './BasicFriend'
+import { UserData } from '../common/interfaces'
+import { fetchAllFriends } from '../apis/apiClient'
 
 export default function Friends() {
+  const fakeFriends = fetchAllFriends() //add await when this becomes an api call
+
+  const pingFriendList = fakeFriends.filter((friend) => friend.ping_active)
+  const otherFriendList = fakeFriends.filter((friend) => !friend.ping_active)
+
+  const renderFriends = (item: UserData) => {
+    if (item.ping_active) {
+      return <ActiveFriend friend={item} />
+    } else {
+      return <BasicFriend friend={item} />
+    }
+  }
+
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <SectionList
+        sections={[
+          { title: 'Active pings', data: pingFriendList },
+          { title: 'Inactive', data: otherFriendList },
+        ]}
+        renderItem={({ item }) => renderFriends(item)}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        keyExtractor={(item) => `basicListEntry-${item.id}`}
+      />
     </View>
   )
 }
+
+// renderItem={({ item }) => <ActiveFriend friend={item} />}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#fde4cf',
+    paddingTop: 22,
+  },
+  mainText: { color: 'black', fontSize: 20, alignSelf: 'center' },
+  image: { width: 100, height: 100 },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingHorizontal: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
--- components/Ping.tsx
+++ components/Ping.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Pressable, StyleSheet, Button } from 'react-native'
 
-export default function Ping() {
+export default function Ping({ navigation }: any) {
+  const handlePress = () => {
+    console.log('pressed')
+    navigation.navigate('Friends')
+  }
   return (
     <View>
-      <Text>Hello</Text>
+      <View>
+        <Text>Hello</Text>
+      </View>
+
+      <Pressable style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>GO TO FRIENDS</Text>
+      </Pressable>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: '#fde4cf',
+  },
+  title: { color: 'black', fontSize: 50, alignSelf: 'center' },
+  mainText: { color: 'black', fontSize: 20, alignSelf: 'center' },
+  buttonText: { color: 'black', fontSize: 30, alignSelf: 'center' },
+  button: {
+    backgroundColor: '#d0d9b3',
+    padding: 10,
+    borderRadius: 5,
+  },
+  image: { width: 100, height: 100 },
+})
