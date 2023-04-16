e6fc46d6441d358f5c74979b9becdf455c036014
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-16 18:25:52 +1200

    WIP

--- components/Swiper.tsx
+++ components/Swiper.tsx
@@ -1,45 +1,36 @@
-import React from 'react'
-import {
-  StyleSheet,
-  View,
-  Image,
-  ScrollView,
-  Dimensions,
-  Text,
-  StatusBar,
-  Platform,
-} from 'react-native'
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
 import Swiper from 'react-native-swiper'
 
 export default function Swiper() {
   return (
     <View>
-      <Swiper showsButtons={true}>
-        <View>
-          <Image
-            style={styles.img}
-            source={require('../assets/activities/beer.png')}
-          />
-        </View>
-        <View>
-          <Image
-            style={styles.img}
-            source={require('../assets/activities/coffee.png')}
-          />
-        </View>
-        <View>
-          <Image
-            style={styles.img}
-            source={require('../assets/activities/talk.png')}
-          />
-        </View>
-        <View>
-          <Image
-            style={styles.img}
-            source={require('../assets/activities/walk.png')}
-          />
-        </View>
-      </Swiper>
+      {/* <Swiper showsButtons={true}> */}
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../assets/activities/beer.png')}
+        />
+      </View>
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../assets/activities/coffee.png')}
+        />
+      </View>
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../assets/activities/talk.png')}
+        />
+      </View>
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../assets/activities/walk.png')}
+        />
+      </View>
+      {/* </Swiper> */}
     </View>
   )
 }
