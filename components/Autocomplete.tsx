85d6123306bd3bba3412e7717f9f1e2ce5a9493f
Olivia Grace <121794969+olivia-clarkeedwards@users.noreply.github.com>
2023-04-17 19:12:25 +1200

    delete googleapiclient

--- components/Autocomplete.tsx
+++ components/Autocomplete.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import { View, ScrollView } from 'react-native'
-import {
-  GooglePlaceData,
-  GooglePlacesAutocomplete,
-} from 'react-native-google-places-autocomplete'
-import { useRef } from 'react'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 
 export default function AutoComplete() {
-  const placesRef = useRef()
-
-  const handlePress = (data: GooglePlaceData, details = null) => {
-    console.log(data.description)
-  }
-
   return (
     <View>
       <ScrollView>
         <GooglePlacesAutocomplete
           placeholder="Let's meet at..."
-          onPress={() => handlePress}
           fetchDetails={true}
           onFail={(error) => console.error(error)}
           query={{
